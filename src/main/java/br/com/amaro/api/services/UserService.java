1c244f1ca3492750d7827fb3d989e7f4cde2269b
Luis Amaro <jrferamaro@gmail.com>
2022-04-16 15:13:39 -0300

    Finalizando crud

--- src/main/java/br/com/amaro/api/services/UserService.java
+++ src/main/java/br/com/amaro/api/services/UserService.java
@@ -8,8 +8,8 @@ import java.util.List;
 public interface UserService {
 
     User findById(Integer id);
-
     List<User> findAll();
-
     User create(UserDTO userDTO);
+    User update(UserDTO userDTO);
+    void delete(Integer id);
 }
